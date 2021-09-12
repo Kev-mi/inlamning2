bafc7729595a85d6cf75115faa94c4dabfd8043c
KevinMina <90063807+KevinMina@users.noreply.github.com>
2021-09-12 21:07:46 +0200

    Update inlamning_2 v3.1.py

--- inlamning_2 v3.1.py
+++ inlamning_2 v3.1.py
@@ -1,1 +1,63 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+class DataHandler:
+    def __init__(self, plotting_goal : str) -> None:
+        ''' Inititialiser that stores the url to use
+        Args:
+            plotting_goal(str) just a name to make it work
+        Returns:
+            None
+    '''
+
+        self.plotting_goal = plotting_goal
+        self.url = 'https://github.com/NordAxon/ec-python-course/blob/master/assignments/01_inlamningsuppgift_2_data.csv?raw=true'
+
+    def plot_vaccinations(self,country_1,country_2):
+        '''Function that plots using the df arguments
+        Args:
+            country_1(Pandas DataFrame): selected country
+            country_2(Pandas DataFrame): other selected country
+        returns:
+            None
+        '''
+
+        country_2.plot()
+        print(country_1)
+        print(country_2)
+        plt.show()
+        return None
+
+    def _extract_country_data(self,selected_country_1 : str,selected_country_2 : str,selected_stat : str) -> "Pandas DataFrame":
+        '''
+
+        Args:
+            selected_country_1(str): string that selected what country the user wants stat from
+            selected_country_2(str): string that selected what country the user wants stat from
+            selected_stat(str): string that selects what stat the user selected to view
+
+        return:
+            country_1_Df(Pandas DataFrame):
+            country_2_Df(Pandas DataFrame):
+
+        '''
+
+        df = pd.read_csv(for_test.url, index_col="country")
+        country_1 = df.loc[selected_country_1]
+        country_2 = df.loc[selected_country_2]
+        country_1_Df = (country_1[[selected_stat]])
+        country_2_Df = (country_2[[selected_stat]])
+        return country_1_Df,country_2_Df
+
+for_test = DataHandler('vaccine_per_million')
+country_1_Df,country_2_Df = for_test._extract_country_data('Norway',"Peru",'daily_vaccinations_per_million')
+for_test.plot_vaccinations(country_1_Df,country_2_Df)
+
+#parser = argparse.ArgumentParser(description='Sum range')
+#parser.add_argument('--country1', help="enter first country", required=True, type=str)
+#parser.add_argument('--country2', help="enter second country", required=True, type=str)
+#parser.add_argument('--data-flag', help="enter what you want the program to do", required=True, type=str)
+#args = parser.parse_args()
+#args.country1, args.country2
 
