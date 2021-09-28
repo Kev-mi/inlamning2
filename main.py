9dff270f54d49fda73e0e9b06b43d2f3dca03b58
KevinMina <90063807+KevinMina@users.noreply.github.com>
2021-09-28 19:46:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 import argparse
 import datahandler
 
-
-def main(data_flag, country1, country2):
-    country_plotting = DataHandler('vaccine_data_plotting')
-    country_1_Df, country_2_Df = country_plotting._extract_country_data(country1, country2, data_flag)
-    country_plotting.plot_vaccinations(country_1_Df, country_2_Df,country1, country2, data_flag)
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='select from 2 countries and then select from this list of options: daily_vaccinations_per_million, daily_vaccinations_raw , total_vaccinations , daily_vaccinations',)
     parser.add_argument('--country1', help="enter first country", required=True, type=str)
