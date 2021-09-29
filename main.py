e497dde6efc31b2f1a0682fc722f499f5db971bd
KevinMina <90063807+KevinMina@users.noreply.github.com>
2021-09-29 13:49:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datahandler
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='select from 2 countries and then select from this list of options: daily_vaccinations_per_million, daily_vaccinations_raw , total_vaccinations , daily_vaccinations',)
     parser.add_argument('--country1', help="enter first country", required=True, type=str)
-    parser.add_argument('--country2',help="enter second country", required=True, type=str)
+    parser.add_argument('--country2',help="enter second country", required=False, type=str)
     parser.add_argument('--data_flag',help="enter what you want the program to do", required=True, type=str)
     args = parser.parse_args()
     args.country1, args.country2
